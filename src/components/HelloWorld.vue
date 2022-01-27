6a75fcca969c27456015b557fed3670debc18740
ValleLuca <valle.lucaAb@outlook.it>
2022-01-27 14:45:50 +0100

    Inizio

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>ciao</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
